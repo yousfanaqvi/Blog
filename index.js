27fef08331efdeba9f898727c5e08315c79b8320
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2023-02-25 17:10:42 -0500

    version D.5

--- index.js
+++ index.js
@@ -30,17 +30,17 @@ app.use(passport.session());
 const user = require("./api/user");
 app.use("/", user);
 
-// app.use(express.static(path.join(__dirname, "./frontend/build")));
-// app.get("*", function (_, res) {
-//   res.sendFile(
-//     path.join(__dirname, "./frontend/build/index.html"),
-//     function (err) {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
-//     }
-//   );
-// });
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running on port ${port}`));
 
