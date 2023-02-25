5d4c5eb55179eeaa00c8d1a1d7a5f8c083f5b0e6
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2023-02-25 17:07:43 -0500

    version D.6

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const passport=require('passport');
 const path = require("path");
 const cors = require("cors");
 const app = express();
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
